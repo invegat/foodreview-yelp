a26469c55c8f2eb4c9b6e5bf7f1c046cd4900b59
Mark Oliver <mark.c.oliver@gmail.com>
2019-05-11 12:40:51 -0400

    updated packages, configured for local server

--- api/src/actions/actions.jsx
+++ api/src/actions/actions.jsx
@@ -3,20 +3,21 @@ import axios from 'axios';
 export const GET_YELPS = 'GET_YELPS';
 export const GET_YELP = 'GET_YELP';
 
-
+const debug = (process.env.NODE_ENV !== 'production');
+const server = debug ? 'http://localhost:5001' : 'https://foodreview-yelp.herokuapp.com';
 
 export const getYelps = (text, geo, radius) => {
   const query = `?term=${text.replace(/\s+/,'+')}&latitude=${geo.latitude}
   &longitude=${geo.longitude}&radius=${radius}`
   // console.log(`getYelps query: ${query}`);
-  const promise = axios.get(`https://foodreview-yelp.herokuapp.com/yelp${query}`);
+  const promise = axios.get(`${server}/yelp${query}`);
   return {
     type: GET_YELPS,
     payload: promise
   };
 };
 export const getYelp =  ( _id) => {
-  const promise = axios.get(`https://foodreview-yelp.herokuapp.com/yelp/${_id}`);
+  const promise = axios.get(`${server}/yelp/${_id}`);
   return {
     type: GET_YELP,
     payload: promise
